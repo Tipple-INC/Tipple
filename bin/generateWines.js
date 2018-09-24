6329c2f790cfd1d191fa256416c8fa78394b7625
Francis Wardley <fwardley90@gmail.com>
2018-09-24 16:49:27 +0200

    data added

--- bin/generateWines.js
+++ bin/generateWines.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Wine = require("../models/Wine");
+var axios = require('axios')
 
 
 mongoose
@@ -11,99 +12,129 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-let wines = [
-  {
-    "wineName": "Bodegas Muga, Prado Enea' Gran Reserva, Rioja",
-    "region": "Rioja",
-    "color": "Red",
-    "vintage": "2005",
-    "score": 93.84,
-},
-{
-    "wineName": "Torres, Salmos, Priorat",
-    "region": "Priorat",
-    "color": "Red",
-    "vintage": "2014",
-    "score": 91.65,
-},
-{
-    "wineName": "Numanthia, Termes, Toro",
-    "region": "Toro",
-    "color": "Red",
-    "vintage": "2004",
-    "score": 92.69,
-},
-{
-    "wineName": "La Rioja Alta, Gran Reserva 904, Rioja",
-    "region": "Rioja",
-    "color": "Red",
-    "vintage": "1997",
-    "score": 93.58,
-},
-{
-    "wineName": "Torres, Mas La Plana Cabernet Sauvignon, Penedes",
-    "region": "Penedes",
-    "color": "Red",
-    "vintage": "2012",
-    "score": 93.94,
-},
-{
-    "wineName": "Torres, Grans Muralles, Conca De Barbera",
-    "region": "Conca De Barbera",
-    "color": "Red",
-    "vintage": "2010",
-    "score": 92.64,
-},
-{
-    "wineName": "Bodegas Aalto, Ribera Del Duero",
-    "region": "Ribera Del Duero",
-    "color": "Red",
-    "vintage": "2014",
-    "score": 92.99,
-},
-{
-    "wineName": "Vega Sicilia, Tinto Valbuena 5, Ribera Del Duero",
-    "region": "Ribera Del Duero",
-    "color": "Red",
-    "vintage": "2012",
-    "score": 93.55,
-},
-{
-    "wineName": "Dominio De Pingus, Ribera Del Duero",
-    "region": "Ribera Del Duero",
-    "color": "Red",
-    "vintage": "2014",
-    "score": 98.83,
-},
-{
-    "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
-    "region": "Rioja",
-    "color": "Red",
-    "vintage": "1985",
-    "score": 95.1,
-},
-{
-    "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
-    "region": "Rioja",
-    "color": "Red",
-    "vintage": "1990",
-    "score": 93.33,
-}  
-]
+// let wines = [
+//   {
+//     "wineName": "Bodegas Muga, Prado Enea' Gran Reserva, Rioja",
+//     "region": "Rioja",
+//     "color": "Red",
+//     "vintage": "2005",
+//     "score": 93.84,
+// },
+// {
+//     "wineName": "Torres, Salmos, Priorat",
+//     "region": "Priorat",
+//     "color": "Red",
+//     "vintage": "2014",
+//     "score": 91.65,
+// },
+// {
+//     "wineName": "Numanthia, Termes, Toro",
+//     "region": "Toro",
+//     "color": "Red",
+//     "vintage": "2004",
+//     "score": 92.69,
+// },
+// {
+//     "wineName": "La Rioja Alta, Gran Reserva 904, Rioja",
+//     "region": "Rioja",
+//     "color": "Red",
+//     "vintage": "1997",
+//     "score": 93.58,
+// },
+// {
+//     "wineName": "Torres, Mas La Plana Cabernet Sauvignon, Penedes",
+//     "region": "Penedes",
+//     "color": "Red",
+//     "vintage": "2012",
+//     "score": 93.94,
+// },
+// {
+//     "wineName": "Torres, Grans Muralles, Conca De Barbera",
+//     "region": "Conca De Barbera",
+//     "color": "Red",
+//     "vintage": "2010",
+//     "score": 92.64,
+// },
+// {
+//     "wineName": "Bodegas Aalto, Ribera Del Duero",
+//     "region": "Ribera Del Duero",
+//     "color": "Red",
+//     "vintage": "2014",
+//     "score": 92.99,
+// },
+// {
+//     "wineName": "Vega Sicilia, Tinto Valbuena 5, Ribera Del Duero",
+//     "region": "Ribera Del Duero",
+//     "color": "Red",
+//     "vintage": "2012",
+//     "score": 93.55,
+// },
+// {
+//     "wineName": "Dominio De Pingus, Ribera Del Duero",
+//     "region": "Ribera Del Duero",
+//     "color": "Red",
+//     "vintage": "2014",
+//     "score": 98.83,
+// },
+// {
+//     "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
+//     "region": "Rioja",
+//     "color": "Red",
+//     "vintage": "1985",
+//     "score": 95.1,
+// },
+// {
+//     "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
+//     "region": "Rioja",
+//     "color": "Red",
+//     "vintage": "1990",
+//     "score": 93.33,
+// }  
+// ]
+const token = "fasdfasdf"
+const instance = axios.create({
+    baseURL: 'https://api.globalwinescore.com',
+    headers: {'Authorization': `Token 554c6f65c5ea3a2902f84409bdde855d684079a4`}
+  })
+
+//   const api_key: process.env.API_KEY
+
 
 Wine.deleteMany()
-.then(() => {
-  return Wine.create(wines)
+.then(() => instance.get('/globalwinescores/latest/?country=Spain'))
+.then(response => {
+    const info = response.data.results;
+    return info.map(e => ({
+        wineName: e.wine,
+        region: e.appellation,
+        color: e.color,
+        vintage: e.vintage,
+        score: e.score
+    }));
 })
-.then(winesCreated => {
-  console.log(`${winesCreated.length} wines created with the following id:`);
-  console.log(winesCreated.map(u => u._id));
+.then( wineData => {
+    console.log(wineData)
+    return Wine.create(wineData);
 })
-.then(() => {
-  // Close properly the connection to Mongoose
-  mongoose.disconnect()
+.then( wines => {
+    console.log(`Created ${wines.length} wines in database`);
+    mongoose.connection.close();
 })
-.catch(err => {
-  mongoose.disconnect()
-  throw err
+.catch(e => {
+    console.log(e)
+    mongoose.connection.close();
 })
+
+
+// .then(response => {
+//   console.log(`${response.length} wines created with the following id:`);
+//   console.log(response.map(u => u._id));
+// })
+// .then(() => {
+//   // Close properly the connection to Mongoose
+//   mongoose.disconnect()
+// })
+// .catch(err => {
+//   mongoose.disconnect()
+//   throw err
+// })
--- bin/seeds.js
+++ bin/seeds.js
@@ -19,14 +19,83 @@ mongoose
   });
 
 let users = [
-  {
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
-  },
-  {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
-  }
+{
+    "wineName": "Bodegas Muga, Prado Enea' Gran Reserva, Rioja",
+    "region": "Rioja",
+    "color": "Red",
+    "vintage": "2005",
+    "score": 93.84,
+},
+{
+    "wineName": "Torres, Salmos, Priorat",
+    "region": "Priorat",
+    "color": "Red",
+    "vintage": "2014",
+    "score": 91.65,
+},
+{
+    "wineName": "Numanthia, Termes, Toro",
+    "region": "Toro",
+    "color": "Red",
+    "vintage": "2004",
+    "score": 92.69,
+},
+{
+    "wineName": "La Rioja Alta, Gran Reserva 904, Rioja",
+    "region": "Rioja",
+    "color": "Red",
+    "vintage": "1997",
+    "score": 93.58,
+},
+{
+    "wineName": "Torres, Mas La Plana Cabernet Sauvignon, Penedes",
+    "region": "Penedes",
+    "color": "Red",
+    "vintage": "2012",
+    "score": 93.94,
+},
+{
+    "wineName": "Torres, Grans Muralles, Conca De Barbera",
+    "region": "Conca De Barbera",
+    "color": "Red",
+    "vintage": "2010",
+    "score": 92.64,
+},
+{
+    "wineName": "Bodegas Aalto, Ribera Del Duero",
+    "region": "Ribera Del Duero",
+    "color": "Red",
+    "vintage": "2014",
+    "score": 92.99,
+},
+{
+    "wineName": "Vega Sicilia, Tinto Valbuena 5, Ribera Del Duero",
+    "region": "Ribera Del Duero",
+    "color": "Red",
+    "vintage": "2012",
+    "score": 93.55,
+},
+{
+    "wineName": "Dominio De Pingus, Ribera Del Duero",
+    "region": "Ribera Del Duero",
+    "color": "Red",
+    "vintage": "2014",
+    "score": 98.83,
+},
+{
+    "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
+    "region": "Rioja",
+    "color": "Red",
+    "vintage": "1985",
+    "score": 95.1,
+},
+{
+    "wineName": "La Rioja Alta, Vina Ardanza Reserva, Rioja",
+    "region": "Rioja",
+    "color": "Red",
+    "vintage": "1990",
+    "score": 93.33,
+}
 ]
 
 User.deleteMany()
