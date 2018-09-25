d5f1df0018d2b0e6ccedac80bde7e88c8e57336b
wergilia <weronika.zdrojewska@gmail.com>
2018-09-25 13:51:51 +0200

    Cloudinary setup

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
+const uploadCloud = require("../config/cloudinary.js");
 
 
-// router.get('/form', (req, res, next) => {
-//   res.render('forms/form');
-// });
+ router.get('/profile', (req, res, next) => {
+   res.render('user/profile');
+});
 
+router.post('/profile', uploadCloud.single('photo'), (req, res, next) => {
+  const { title, description } = req.body;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+  const newMovie = new Movie({title, description, imgPath, imgName})
+  newMovie.save()
+  .then(movie => {
+    res.redirect('/')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 
